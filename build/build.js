7caf79838ee956fcf2d648b8c8ce6835191ed9a3
tcly861204 <tcly861204@hotmail.com>
2018-12-25 23:17:36 +0800

    build

---
+++ build/build.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack')
+const ora = require('ora')
+const rm = require('rimraf')
+const path = require('path')
+
+const webpackDevConfig = require('./webpack.config.prod')
+const spinner = ora('building for development...')
+spinner.start()
+rm(path.join(__dirname, '../dist'), err=>{
+  if (err) throw err
+  webpack(webpackDevConfig, function(err, stats){
+    spinner.stop()
+    process.stdout.write(stats.toString({
+      colors: true,
+      modules: false,
+      children: false,
+      chunks: false,
+      chunkModules: false
+    }) + '\n\n' )
+  })
+})
---
+++ build/webpack.config.prod.js
@@ -0,0 +1,16 @@
+const path = require('path')
+function resolve(dir){
+  return path.resolve(__dirname,'..', dir)
+}
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: resolve('src/main.js')
+  },
+  output: {
+    filename: 'js/[name].[hash:8].js',
+    path: resolve('dist/'),
+    publicPath: '/'
+  }
+}
