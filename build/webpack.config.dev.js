68da770ae58c703f4a951d364417f333babd09a9
tcly861204 <tcly861204@hotmail.com>
2018-12-26 21:06:23 +0800

    merge

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -1,10 +1,10 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const cleanWebpackPlugin = require('clean-webpack-plugin')
 const webpack = require('webpack')
 function resolve(dir){
-  return path.resolve(__dirname,'..', dir)
+  return path.resolve(__dirname, '..', dir)
 }
-
 module.exports = {
   mode: 'development',
   entry: {
@@ -12,21 +12,60 @@ module.exports = {
   },
   output: {
     filename: 'js/[name].[hash:8].js',
-    path: resolve('dist/'),
-    publicPath: '/'
+    path: resolve('dist'),
+    publicPath: '/',
+    chunkFilename:'js/[name].[chunkhash:8].js'
   },
   devServer: {
-    contentBase: resolve('dist/'),
+    contentBase: resolve('public/'),
+    historyApiFallback: true,
     host: '0.0.0.0',
+    open: true,
+    inline: true,
+    port: 8000,
+    compress: true, //服务器返回给浏览器的时候是否启动gzip压缩
+  },
+  module: {
+    unknownContextCritical : false,
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      minChunks: 1,
+      minSize: 0,
+      cacheGroups: {
+        framework: {
+          test: "framework",
+          name: "framework",
+          enforce: true
+        }
+      }
+    }
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': '"development"'
+    new cleanWebpackPlugin('dist', {
+      root: resolve('/'),
+      verbose: false,
+      dry: false
     }),
     new HtmlWebpackPlugin({
-      filename: 'index.html',
       template: resolve('public/index.html'),
-      inject: true
-    }),
+      filename: 'index.html',
+      favicon: resolve('public/favicon.ico'),
+      inject: 'body',
+      title: 'Webpack App',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+      }
+    })
   ]
 }
