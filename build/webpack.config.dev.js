899614695ac89f0280511195441051a28795455e
tcly861204 <tcly861204@hotmail.com>
2018-12-26 08:37:58 +0800

    merge

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 function resolve(dir){
   return path.resolve(__dirname,'..', dir)
 }
@@ -12,5 +14,19 @@ module.exports = {
     filename: 'js/[name].[hash:8].js',
     path: resolve('dist/'),
     publicPath: '/'
-  }
+  },
+  devServer: {
+    contentBase: resolve('dist/'),
+    host: '0.0.0.0',
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': '"development"'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: resolve('public/index.html'),
+      inject: true
+    }),
+  ]
 }
