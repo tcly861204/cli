051a6132c2f819dd37272b6c3005667e41f7d674
tcly861204 <tcly861204@hotmail.com>
2018-12-27 08:38:41 +0800

    修复配置

--- build/webpack.config.prod.js
+++ build/webpack.config.prod.js
@@ -1,6 +1,9 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const cleanWebpackPlugin = require('clean-webpack-plugin')
+const webpack = require('webpack')
 function resolve(dir){
-  return path.resolve(__dirname,'..', dir)
+  return path.join(__dirname,'..', dir)
 }
 
 module.exports = {
@@ -10,7 +13,55 @@ module.exports = {
   },
   output: {
     filename: 'js/[name].[hash:8].js',
-    path: resolve('dist/'),
+    path: resolve('/dist'),
     publicPath: '/'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: /node_modules/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        options: {
+          formatter: require('eslint-friendly-formatter')
+        }
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: 10000,
+          name: 'img/[name].[hash:7].[ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': 'production'
+    }),
+    new cleanWebpackPlugin('dist', {
+      root: resolve('/'),
+      verbose: false,
+      dry: false
+    }),
+    new HtmlWebpackPlugin({
+      template: resolve('public/index.html'),
+      filename: 'index.html',
+      favicon: resolve('public/favicon.ico'),
+      inject: 'body',
+      title: 'Webpack App',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+      }
+    })
+  ]
 }
